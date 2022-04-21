3b4cce00d259173c70362ee8fef3b5701f22cab3
Oliwia <oliwia.lubiarz@widelab.co>
2022-04-21 21:21:34 +0200

    nav and footer

--- flashcard/src/components/Footer/Footer.tsx
+++ flashcard/src/components/Footer/Footer.tsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
-import React from 'react'
-
 function Footer() {
-  return (
-    <div className={styles.footer}>Footer</div>
-  )
+ return <div className={styles.footer}>Footer</div>;
 }
 
-export default Footer
+export default Footer;
